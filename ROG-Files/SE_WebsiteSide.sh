c0f6465f17883a13b8a6c7863bc8612c678c9416
Adrian Jann Octat <31035460+Dreyannz@users.noreply.github.com>
2018-06-06 23:27:14 +0800

    Add files via upload

---
+++ ROG-Files/SE_WebsiteSide.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Script Author: _Dreyannz_
+# SoftEther Website-Side Script
+# Scheduled Every 20 Mins
+
+REQ1="ROG-PrivateSE1.sh";
+REQ2="ROG-PrivateSE2.sh";
+REQ3="ROG-PrivateSE3.sh";
+GITHUB1="https://raw.githubusercontent.com/Dreyannz/SEAutoScript/master/ROG-Files/ROG-PrivateSE1.sh";
+GITHUB1="https://raw.githubusercontent.com/Dreyannz/SEAutoScript/master/ROG-Files/ROG-PrivateSE2.sh";
+GITHUB1="https://raw.githubusercontent.com/Dreyannz/SEAutoScript/master/ROG-Files/ROG-PrivateSE3.sh";
+LOCATION="/home/vps/public_html/softether";
+PENDING="status_pending.txt";
+APPROVED="status_approved.txt";
+TEMP="temp.txt";
+
+sleep 15
+cd $LOCATION
+rm $REQ1
+rm $REQ2
+rm $REQ3
+wget -O $REQ1 "$GITHUB1"
+wget -O $REQ2 "$GITHUB2"
+wget -O $REQ3 "$GITHUB3"
+cat  $PENDING $APPROVED > $TEMP
+mv $TEMP $APPROVED
+clear
