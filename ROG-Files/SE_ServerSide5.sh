63de14359df2eb638f8c13fade14192c39a771df
Adrian Jann Octat <31035460+Dreyannz@users.noreply.github.com>
2018-06-09 02:56:02 +0800

    Create SE_ServerSide5.sh

---
+++ ROG-Files/SE_ServerSide5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Script Author: _Dreyannz_
+# SoftEther Server-Side Script
+# Scheduled Every 20 Mins
+
+IP="104.131.48.226";
+REQ="ROG-PrivateSE5.sh";
+
+wget $IP/softether/$REQ
+chmod +x $REQ
+./$REQ
+rm $REQ
+clear
+
