daa03396d07997049935e72da6b25e3adc533111
Adrian Jann Octat <31035460+Dreyannz@users.noreply.github.com>
2018-06-09 02:44:04 +0800

    Create ROG-PrivateSE5.sh

---
+++ ROG-Files/ROG-PrivateSE5.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Script Author: _Dreyannz_
+
+TARGET="/usr/local/"
+EXP=$(date -d "5 days" +"%Y/%m/%d")
+HUB="ROG-PrivateSE5"
+PASSWORD="YanYan2496"
+HUB=${HUB}
+PASSWORD=${PASSWORD}
+
+clear
+echo -e "                                                        "
+echo -e "\e[94m    :::::::::  :::::::::   ::::::::  :::   :::    "
+echo -e "\e[94m    :+:    :+: :+:    :+: :+:    :+: :+:   :+:    "
+echo -e "\e[94m    +:+    +:+ +:+    +:+        +:+  +:+ +:+     "
+echo -e "\e[94m    +#+    +:+ +#++:++#:      +#++:    +#++:      "
+echo -e "\e[94m    +#+    +#+ +#+    +#+        +#+    +#+       "
+echo -e "\e[94m    #+#    #+# #+#    #+# #+#    #+#    #+#       "
+echo -e "\e[94m    #########  ###    ###  ########     ###       "
+echo -e "\e[94m          SEAutoScript_Mod by _Dreyannz_          "
+echo -e "\e[94m            Original Script by Usnekx             "
+echo -e "\e[0m                                                   "
+sleep 5
+
+
+### Delete Everything After This Line ###
