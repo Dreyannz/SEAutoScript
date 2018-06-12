776a6330bb07e1708f0851345b866af8688dddd3
Adrian Jann Octat <31035460+Dreyannz@users.noreply.github.com>
2018-06-12 17:09:34 +0800

    Add files via upload

---
+++ SecretFormula.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Script Author: _Dreyannz_
+
+iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -s 192.168.7.0/24 -j ACCEPT
+iptables -A FORWARD -j REJECT
+iptables -t nat -A POSTROUTING -s 192.168.7.0/24 -j SNAT --to-source MYIP
