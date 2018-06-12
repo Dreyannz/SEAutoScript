60e09d2045dfc6ed67c39d07c905a9a9995a4888
Adrian Jann Octat <31035460+Dreyannz@users.noreply.github.com>
2018-06-13 01:54:08 +0800

    Delete SecretFormula.sh

--- SecretFormula.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script Author: _Dreyannz_
-
-iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -s 192.168.7.0/24 -j ACCEPT
-iptables -A FORWARD -j REJECT
-iptables -t nat -A POSTROUTING -s 192.168.7.0/24 -j SNAT --to-source xxxxxxxxx
