df4147af61a86a3941c50cf5da15057dc2dfe95d
Adrian Jann Octat <31035460+Dreyannz@users.noreply.github.com>
2018-06-12 18:55:49 +0800

    Update SecretFormula.sh

--- SecretFormula.sh
+++ SecretFormula.sh
@@ -4,4 +4,4 @@
 iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -s 192.168.7.0/24 -j ACCEPT
 iptables -A FORWARD -j REJECT
-iptables -t nat -A POSTROUTING -s 192.168.7.0/24 -j SNAT --to-source MYIP
+iptables -t nat -A POSTROUTING -s 192.168.7.0/24 -j SNAT --to-source VPSIPHERE
