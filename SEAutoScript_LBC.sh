ae6dcbd2c540c0279dd63516eec968f0be6e0bbe
Adrian Jann Octat <31035460+Dreyannz@users.noreply.github.com>
2018-06-12 16:22:16 +0800

    Add files via upload

--- SEAutoScript_LBC.sh
+++ SEAutoScript_LBC.sh
@@ -25,7 +25,7 @@ echo -e "\e[94m    #########  ###    ###  ########     ###       "
 echo -e "\e[94m          SEAutoScript_Mod by _Dreyannz_          "
 echo -e "\e[94m                  ==LBC Setup==                   "
 echo -e "\e[94m                                                   "
-echo -n "    Server IP             : " ; read -p '$MYIP' -e -i HOST
+echo -n "    Server IP             : " ; read -p '' -e -i $MYIP HOST
 echo -e "                                                   "
 echo -n '    Virtual Hub Name      : ' ; read trial
 echo -e "                                                 "
